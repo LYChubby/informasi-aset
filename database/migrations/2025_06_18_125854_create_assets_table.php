60438f2a30187ee38ac549ee5e840766ca69f3d7
muhfarrasm <muhammadfarrasm@gmail.com>
2025-06-18 20:25:19 +0700

    ADD - Model asetIT dan Migrationnya

---
+++ database/migrations/2025_06_18_125854_create_assets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('asets', function (Blueprint $table) {
+            $table->id();
+            $table->date('tgl_terima');
+            $table->string('kondisi');
+            $table->string('status');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // relasi ke users
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('asets');
+    }
+};
