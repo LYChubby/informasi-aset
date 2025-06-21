738965454dfd211aaa08b89da54bff4fe4b4c89d
muhfarrasm <muhammadfarrasm@gmail.com>
2025-06-21 17:51:02 +0700

    CREATE ADMIN

--- routes/web.php
+++ routes/web.php
@@ -11,32 +11,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('auth')->group(function () {
-// Route dashboard yang sudah disatukan
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route yang membutuhkan autentikasi
+Route::middleware(['auth', 'verified'])->group(function () {
 
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    // Resource Routes
+    // Asset dan Report bisa diakses oleh semua user
     Route::resource('assets', AssetController::class);
     Route::resource('reports', ReportController::class);
-    
+    Route::put('/reports/{id}/update-status', [ReportController::class, 'updateStatus'])->name('reports.updateStatus');
 
-    Route::prefix('admin')->name('admin.')->middleware('can:admin')->group(function() {
-    Route::resource('users', UserManagementController::class);
-});
-    
-    // Profile Routes
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-
-    // Report Routes
-
-    Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
-    Route::put('/reports/{id}/update-status', [ReportController::class, 'updateStatus'])->name('reports.updateStatus');
+    // Khusus Admin
+    Route::middleware('can:admin')->prefix('admin')->name('admin.')->group(function () {
+        Route::resource('users', UserManagementController::class);
+    });
 });
 
 require __DIR__.'/auth.php';
