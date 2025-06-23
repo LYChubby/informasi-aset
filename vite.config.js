fc6691332b98467bd5c4ca01e85c366d5200a730
muhfarrasm <muhammadfarrasm@gmail.com>
2025-06-23 16:39:32 +0700

    merger

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-<<<<<<< HEAD
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/delete-confirm.js'],
-=======
-            input: ["resources/css/app.css", "resources/js/app.js"],
->>>>>>> 5260114762614247690f54a1210fd6ca68acf199
             refresh: true,
         }),
     ],
